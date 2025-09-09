f43545863ebaa7d83cbff97ae32a86be6306a4ad
Jackson Frazier <jackygfraz@gmail.com>
2025-09-09 15:54:47 -0500

    Login page created. Needs review and adjustments

--- hippo_exchange_mobile_app/lib/main.dart
+++ hippo_exchange_mobile_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  
   runApp(const MainApp());
 }
 
@@ -9,12 +10,174 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // removes debug banner
       home: Scaffold(
+        appBar: AppBar(
+          title:  Text(
+            'Hippo Enterprise',
+            style: TextStyle(
+              shadows: [
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.black,
+                offset: Offset(0, 0.3),
+              ),
+              ],
+              fontSize: 40.0, // Adjust the font size as needed
+              fontWeight: FontWeight.bold,
+
+            ),
+            
+          ),
+          centerTitle: true,
+          
+        ),
+        
         body: Center(
-          child: Text('Hello World!'),
+        child: Container(
+          alignment: Alignment.topCenter,
+          width: 350,
+          padding: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            //border: Border.all(color: Colors.blueGrey, width: 1.5), // used for testing container size
+            borderRadius: BorderRadius.circular(16),
+           
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 150),
+
+
+              // Username text above field
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Username",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+              ),
+              const SizedBox(height: 8),
+              
+              // Username text field
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Username",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                ),
+              ),
+              const SizedBox(height: 20),
+
+
+              // Password text above field
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Password",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+              ),
+              const SizedBox(height: 8),
+
+              // Password field box 
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[50],
+              
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child:ElevatedButton(
+                    
+                  style: ElevatedButton.styleFrom(
+                 
+                    backgroundColor: Colors.blueGrey[800],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onPressed: (){},
+                  child: Text("Login", style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20
+                    ),
+                  )
+                )
+              ),
+            const SizedBox(height: 10),
+            // HYPERLINK BUTTON FOR REGISTERATION 
+            //  INSTEAD OF BUTTON OPTION
+            // TextButton( onPressed: () {
+              
+            //   },
+            //   child: Text(
+            //        "Dont Have and Account?",
+            //         style: TextStyle(
+            //           decoration: TextDecoration.underline,
+            //           color: Colors.grey[700],
+            //           fontSize: 15,
+            //         ),
+            //       )
+            
+            // )
+            
+            // Text below login field
+            Text(
+              "Dont Have and Account?",
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+                color: Colors.grey[700],
+                fontSize: 16,
+
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // Registration button  
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey[800],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Register",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+                const SizedBox(height: 10),
+
+      
+
+            ],
+
+          ),
         ),
       ),
+          
+      
+      )
     );
+    
   }
 }
