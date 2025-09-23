d18c5844a24a386ee775e7c55bf6fe674f046f60
Jackson Frazier <jackygfraz@gmail.com>
2025-09-23 15:58:37 -0500

    init commit for leningPage

---
+++ hippo_exchange_mobile_app/lib/pages/lendingPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:hippo_exchange_mobile_app/auth_service.dart';
+
+
+class Lendingpage extends StatefulWidget{
+    const Lendingpage({super.key});
+
+  @override
+  State<Lendingpage> createState() => _LendingPageState();
+
+}
+
+class _LendingPageState extends State<Lendingpage>{
+  
+}
