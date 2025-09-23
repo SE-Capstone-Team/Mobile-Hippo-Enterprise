139b9891edb792813ea124dc7f30c716284c4144
Jackson Frazier <jackygfraz@gmail.com>
2025-09-23 13:36:13 -0500

    removed a dupe gotoregistration

--- hippo_exchange_mobile_app/lib/main.dart
+++ hippo_exchange_mobile_app/lib/main.dart
@@ -33,11 +33,7 @@ class _BasePageState extends State<BasePage> {
       _currentIndex = 1;
     });
   }
-  void _goToRegistration() {
-    setState(() {
-      _currentIndex = 1;
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
